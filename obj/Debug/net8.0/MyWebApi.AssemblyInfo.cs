b2fdaf50f24d2a463e9b506be76201095049c81b
Nimmy Sam <Nimmy.R.Sam@ie.ey.com>
2024-05-28 11:24:28 +0100

    sixth commit

--- obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03318eab8123964274442166b85c57e115cb3e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e91073f003601098dad4b76a3e0583d64db3be19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
